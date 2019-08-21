0586d5379c276d6a6ea9e1db59c24920869958ee
AngelETrujillo <18.eduaard@gmail.com>
2019-08-21 00:02:34 -0500

    commit inicial

---
+++ landingPersonal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="landingPersonal.MvcApplication" Language="C#" %>
---
+++ landingPersonal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(landingPersonal.Startup))]
+namespace landingPersonal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
